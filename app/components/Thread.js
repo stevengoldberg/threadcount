96dbfe56c384cc36b6c853db5b957d178772f8cf
Steve Goldberg <steve.schnee@gmail.com>
2018-08-09 17:48:59 -0700

    Make threads look nicer

--- app/components/Thread.js
+++ app/components/Thread.js
@@ -3,19 +3,34 @@ import React, { Component } from 'react';
 import styles from './Thread.css';
 import decodeHtml from '../utils/decode-html';
 
+type Message = {
+  internalDate: string
+};
+
 type Props = {
   thread: Object,
   index: number,
-  style: Object
+  style: Object,
+  messages?: Array<Message>
 };
 
 export default class Thread extends Component<Props> {
   props: Props;
 
+  static defaultProps = {
+    messages: []
+  };
+
   render() {
-    const { thread, style, index } = this.props;
+    const { thread, style, index, messages } = this.props;
     const oddColor = '#92b5e8';
     const evenColor = '#88a4ce';
+    const getDate = message => new Date(parseInt(message.internalDate, 10));
+    const getFormattedDate = message => getDate(message).toLocaleDateString();
+    const displayDate = messages.length ? getFormattedDate(messages[0]) : null;
+    const displayText = thread.snippet
+      ? decodeHtml(thread.snippet)
+      : '{{ no preview }}';
 
     return (
       <div
@@ -25,7 +40,11 @@ export default class Thread extends Component<Props> {
         }}
         className={styles.content}
       >
-        {decodeHtml(thread.snippet)}
+        <span className={styles.date}>{displayDate}</span>
+        {displayText}
+        {messages.legnth && (
+          <span className={styles.count}>[{messages.length}]</span>
+        )}
       </div>
     );
   }
