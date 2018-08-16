eca30856e35e03f699070d40c00ca00347806b32
Steve Goldberg <steve.schnee@gmail.com>
2018-08-16 12:04:45 -0700

    Calculate word counts

--- app/containers/Ratio.js
+++ app/containers/Ratio.js
@@ -1,5 +1,7 @@
 import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
+import { decode, isUrlSafeBase64 } from 'url-safe-base64';
+import base64js from 'base64-js';
 import pickBy from 'lodash/pickBy';
 import find from 'lodash/find';
 import reduce from 'lodash/reduce';
@@ -40,6 +42,25 @@ const isMessageMine = (myEmail, message) => {
   return fromValue.indexOf(myEmail) > -1;
 };
 
+const getWordCount = message => {
+  const decodeHTML = (str, encoding = 'utf-8') => {
+    const bytes = base64js.toByteArray(str);
+    return new TextDecoder(encoding).decode(bytes);
+  };
+  const messageData = get(message, 'payload.body.data', '');
+  let decodedMessage;
+  if (isUrlSafeBase64(messageData) && decode(messageData) !== messageData) {
+    decodedMessage = decode(messageData);
+  } else {
+    try {
+      decodedMessage = atob(messageData);
+    } catch (e) {
+      decodedMessage = decodeHTML(messageData);
+    }
+  }
+  return decodedMessage === '' ? 0 : decodedMessage.split(' ').length;
+};
+
 function calculateRatio(messages, myEmail) {
   return reduce(
     messages,
@@ -47,18 +68,22 @@ function calculateRatio(messages, myEmail) {
       if (isMessageMine(myEmail, message)) {
         return {
           ...acc,
-          myMessages: acc.myMessages + 1
+          myMessages: acc.myMessages + 1,
+          myWords: acc.myWords + getWordCount(message)
         };
       }
       return {
         ...acc,
-        theirMessages: acc.theirMessages + 1
+        theirMessages: acc.theirMessages + 1,
+        theirWords: acc.theirWords + getWordCount(message)
       };
     },
     {
       totalMessages: messages.length,
       myMessages: 0,
-      theirMessages: 0
+      myWords: 0,
+      theirMessages: 0,
+      theirWords: 0
     }
   );
 }
