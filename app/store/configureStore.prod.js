f63b5221ab83a5c5d1d2651da7850a0e77ff219a
Steve Goldberg <steve.schnee@gmail.com>
2018-08-21 14:08:35 -0700

    Configure prod store

--- app/store/configureStore.prod.js
+++ app/store/configureStore.prod.js
@@ -1,15 +1,30 @@
 // @flow
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import { apiMiddleware } from 'redux-api-middleware';
 import { createHashHistory } from 'history';
 import { routerMiddleware } from 'react-router-redux';
 import rootReducer from '../reducers';
+import authMiddleware from '../middleware/auth';
+import storageMiddleware from '../middleware/storage';
+import contactsMiddleware from '../middleware/contacts';
 
 const history = createHashHistory();
 const router = routerMiddleware(history);
-const enhancer = applyMiddleware(thunk, router);
 
 function configureStore(initialState) {
+  const middleware = [];
+  const enhancers = [];
+  middleware.push(
+    thunk,
+    authMiddleware,
+    apiMiddleware,
+    storageMiddleware,
+    contactsMiddleware,
+    router
+  );
+  enhancers.push(applyMiddleware(...middleware));
+  const enhancer = compose(...enhancers);
   return createStore(rootReducer, initialState, enhancer);
 }
 
